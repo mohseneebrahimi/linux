71f3c02ed409890861557ecd1c9591c95ba8450b
mohseneebrahimi <mohsen.eebrahimi@gmail.com>
2026-02-01 22:59:29 +0100

    Update PersonController.java

--- demo4/src/main/java/com/example/demo4/personControlle/PersonController.java
+++ demo4/src/main/java/com/example/demo4/personControlle/PersonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 	@GetMapping("/test")
 	public String sayHello() {
-		return "Helloooo";	
+		return "Helloooo2222";	
 	}
 }
 
