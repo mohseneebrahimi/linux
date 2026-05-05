cab460265cb0d00f5c3d40992985e8d154714e91
mohseneebrahimi <mohsen.eebrahimi@gmail.com>
2026-05-05 12:49:21 +0200

    m1

--- demo4/src/main/java/com/example/demo4/personControlle/PersonController.java
+++ demo4/src/main/java/com/example/demo4/personControlle/PersonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 	@GetMapping("/test")
 	public String sayHello() {
-		return "Helloooo222200000000xxxxxxxgiiiiii";	
+		return "HellooooKirr";	
 	}
 }
 
